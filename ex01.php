5b69008d936ba460b9a0338d8f9b1fb25dc43972
chutikarn777 <144976248+chutikarn777@users.noreply.github.com>
2023-09-14 12:03:07 +0700

    Add files via upload

---
+++ ex01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <?php
+    function test ($str)
+    { $s = substr($str,strlen($str)-1);
+     return $s.$str.$s;
+    }
+
+    echo test("red"),"<br>";
+    echo test("green"),"<br>";
+    echo test("1"),"<br>";
+    ?>
+
+
+</body>
+
+</html
